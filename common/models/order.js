842293ac985b995efd2ce3f221ab2a301fdcfdd1
liudonghua123 <liudonghua123@gmail.com>
2017-08-29 09:05:41 +0800

    fix update item logic

--- common/models/order.js
+++ common/models/order.js
@@ -7,49 +7,22 @@ module.exports = function(Order) {
   // Operation-hooks
   // http://loopback.io/doc/en/lb3/Operation-hooks.html#overview
   // http://apidocs.strongloop.com/loopback/#persistedmodel-updateall
-  // Order.observe('before save', function updateItemIfNecessary(ctx, next) {
-  //   // instance and data are difference, see http://loopback.io/doc/en/lb3/Operation-hooks.html#hook-and-operation-specific-properties
-  //   const instance = ctx.instance ? ctx.instance : ctx.data;
-  //   if (instance && instance.isSended) {
-  //     const count = instance.count;
-  //     const itemId = instance.itemId;
-  //     const Item = Order.app.models.Item;
-  //     Item.findOne({where: {id: itemId}}, function(err, item) {
-  //       if (err) {
-  //         console.error(err);
-  //         next(err);
-  //       }
-  //       if (count > item.amount || count > item.balance) {
-  //         next(new Error('not enough item avaliable'));
-  //       }
-  //       debug('update item');
-  //       item.balance = item.balance - count;
-  //       Item.upsert(item, function(err1, result) {
-  //         if (err1) {
-  //           console.error(err1);
-  //           next(err1);
-  //         }
-  //         debug('begin update order');
-  //         next();
-  //       });
-  //     });
-  //   } else {
-  //     next();
-  //   }
-  // });
-
-  // Remote-hooks
-  // http://loopback.io/doc/en/lb3/Remote-hooks.html#examples
-  // PATCH->patchOrCreate(an alias for updateOrCreate), PUT->replaceOrCreate
-  Order.beforeRemote('**', function(context, modelInstance, next) {
-    debug(context.methodString, 'was invoked remotely');
-    if (context.methodString.endsWith('OrCreate') || context.methodString.endsWith('patchAttributes')) {
-      // check isSended flag
-      const isSended = context.req.body.isSended;
-      const count = context.req.body.count;
-      if (isSended) {
-        // check whether enough item avaliable
-        const itemId = context.req.body.itemId;
+  Order.observe('before save', function updateItemIfNecessary(ctx, next) {
+    // instance and data are difference, see http://loopback.io/doc/en/lb3/Operation-hooks.html#hook-and-operation-specific-properties
+    const instance = ctx.instance ? ctx.instance : ctx.data;
+    if (!instance) {
+      return next();
+    }
+    Order.findOne({where: {id: instance.id}}, function(err, order) {
+      if (err) {
+        console.error(err);
+        next(err);
+      }
+      const previousIsSended = order.isSended;
+      debug('previous isSended is %s', previousIsSended);
+      if (!previousIsSended && instance.isSended) {
+        const count = instance.count;
+        const itemId = instance.itemId;
         const Item = Order.app.models.Item;
         Item.findOne({where: {id: itemId}}, function(err, item) {
           if (err) {
@@ -61,8 +34,7 @@ module.exports = function(Order) {
           }
           debug('update item');
           item.balance = item.balance - count;
-
-          Item.upsert(item, function(err1, newItem) {
+          Item.upsert(item, function(err1, result) {
             if (err1) {
               console.error(err1);
               next(err1);
@@ -74,8 +46,57 @@ module.exports = function(Order) {
       } else {
         next();
       }
-    } else {
-      next();
-    }
+    });
   });
+
+  // Remote-hooks
+  // http://loopback.io/doc/en/lb3/Remote-hooks.html#examples
+  // PATCH->patchOrCreate(an alias for updateOrCreate), PUT->replaceOrCreate
+  // Order.beforeRemote('**', function(context, modelInstance, next) {
+  //   debug(context.methodString, 'was invoked remotely');
+  //   if (context.methodString.endsWith('OrCreate') ||
+  //     context.methodString.endsWith('patchAttributes')) {
+  //     // check isSended flag
+  //     const isSended = context.req.body.isSended;
+  //     const count = context.req.body.count;
+  //     const id = context.req.body.id;
+  //     Order.findOne({where: {id: id}}, function(err, order) {
+  //       if (err) {
+  //         console.error(err);
+  //         next(err);
+  //       }
+  //       const previousIsSended = order.isSended;
+  //       debug('previous isSended is %s', previousIsSended);
+  //       if (!previousIsSended && isSended) {
+  //         // check whether enough item avaliable
+  //         const itemId = context.req.body.itemId;
+  //         const Item = Order.app.models.Item;
+  //         Item.findOne({where: {id: itemId}}, function(err, item) {
+  //           if (err) {
+  //             console.error(err);
+  //             next(err);
+  //           }
+  //           if (count > item.amount || count > item.balance) {
+  //             next(new Error('not enough item avaliable'));
+  //           }
+  //           debug('update item');
+  //           item.balance = item.balance - count;
+  //
+  //           Item.upsert(item, function(err1, newItem) {
+  //             if (err1) {
+  //               console.error(err1);
+  //               next(err1);
+  //             }
+  //             debug('begin update order');
+  //             next();
+  //           });
+  //         });
+  //       } else {
+  //         next();
+  //       }
+  //     });
+  //   } else {
+  //     next();
+  //   }
+  // });
 };
